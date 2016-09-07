18cfcab34cdb5c2fd1b3993574c1a07701524ca4
Nicolas <n.bellamy@titeya.com>
2016-09-07 21:40:48 +0200

    add auth rregistry

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,9 +5,11 @@ set -e
 mkdir -p -m 0700 /root/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n" >> /root/.ssh/config
 
-docker build -t $IMAGEINFOS $GITREPO
+docker build -t $IMAGE_INFOS $GIT_REPO
 
-docker tag $IMAGEINFOS $DOCKERREPO/$IMAGEINFOS
-docker push $DOCKERREPO/$IMAGEINFOS
+docker login --username=$REPO_USER --password=$REPO_PASS --email=$REPO_EMAIL $DOCKER_REPO
+
+docker tag $IMAGE_INFOS $DOCKER_REPO/$IMAGE_INFOS
+docker push $DOCKER_REPO/$IMAGE_INFOS
 
 exec "$@"
